94d32bd455ed697e55fdcbf3ac0dd7d27d6a5db3
Kanav Gupta <kanav0610@gmail.com>
2022-05-28 19:17:14 +0530

    finally we achieved dialogue

--- sample/tree.sol
+++ sample/tree.sol
@@ -1,11 +1,7 @@
 pragma solidity ^0.8.0;
 
 contract Tree {
-    /// @notice Set the tree age to `numYears` years
-    function setAge(uint256 numYears) external {
-        // set the age into storage
-    }
-
+    /// @notice Exchanged `q1` `@tokenName(t1)` for `q2` `@tokenName(t2)`
     function exchange(uint256 q1, address t1, uint256 q2, address t2) external {
         // exchange q1 of t1 for q2 of t2
     }
